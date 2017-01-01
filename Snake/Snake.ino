31c4f2b80924a6095ff1acd2a13829ed1416b30e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-01-01 18:45:55 +1300

    Fixes for movement; added collision detection
    This fixes a number of issues with the movement of the snake, so that it
    is now working correctly.  This also adds collision detection which
    resets the game if a self-loop is encountered.

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -7,10 +7,12 @@ Arduboy arduboy;
 // Config
 // =========================================================
 
-#define NORTH 1
-#define SOUTH 2
-#define EAST 3
-#define WEST 4
+typedef enum Direction { 
+  NORTH, 
+  SOUTH, 
+  EAST,
+  WEST 
+} Direction;
 
 typedef struct Point {
   int x;
@@ -21,7 +23,7 @@ typedef struct Point {
  * Represents a single section within a snake.
  */
 typedef struct Section {
-  unsigned int direction; // NORTH, SOUTH, EAST or WEST
+  Direction direction; // NORTH, SOUTH, EAST or WEST
   unsigned int length;    // number of blocks in this section
 } Section;
 
@@ -62,10 +64,14 @@ Snake snake;
 void setup() {
   arduboy.start();
   arduboy.setFrameRate(30);
+  resetGame();
+}
+
+void resetGame() {
   // Configure the snake
   snake.head = {8,6};
   snake.numberOfSections = 1;
-  snake.sections[0] = { EAST, 1 };
+  snake.sections[0] = { EAST, 5 };
 }
 
 // =========================================================
@@ -81,31 +87,77 @@ void loop() {
     // Check for change of direction
     updateDirection();
     // Move snake in current direction
-    moveSnake();
+    moveSnake();        
     // Draw snake
     arduboy.clearDisplay();
     drawSnake();
     arduboy.display();
+    // Check for self collision
+    if(isTouchingSelf()) {
+      gameOver();
+    }
     //
     delay(1000);
   }
 }
 
+void gameOver() {
+  arduboy.clearDisplay();
+  resetGame();
+  delay(1000);
+}
+
 /**
-   Check whether any buttons are pressed which signals a change of direction.
-*/
+ * Check whether or not the snake is touching itself.
+ */
+bool isTouchingSelf() {
+  Point head = snake.head;
+  Point p = head;
+  for(int i=0;i!=snake.numberOfSections;i++) {
+    Section section = snake.sections[i];
+    // Ignore first section, since the head always
+    // intersects this!
+    if(i != 0 && isPointInSection(head,p,section)) {
+      return true;
+    } else {
+      p = getEndPoint(p,section);
+    }
+  }
+  return false;
+}
+
+bool isPointInSection(Point p, Point from, Section section) {
+  Point to = getEndPoint(from,section);
+  if(p.x == from.x && p.x == to.x) {
+    int s = min(from.y, to.y);
+    int e = max(from.y, to.y);
+    return s <= p.y && p.y <= e;
+  } else if(p.y == from.y && p.y == to.y) {
+    int s = min(from.x, to.x);
+    int e = max(from.x, to.x);
+    return s <= p.x && p.x <= e;
+  } else {
+    return false;
+  }
+}
+
+/**
+ *  Check whether any buttons are pressed which signals a change of direction.
+ */
 void updateDirection() {
-  int oDirection = snake.sections[0].direction;
-  int nDirection;
+  Direction oDirection = getCurrentDirection();
+  Direction nDirection;
   //
   if (arduboy.pressed(LEFT_BUTTON)) {
-    nDirection = EAST;
-  } else if (arduboy.pressed(RIGHT_BUTTON)) {
     nDirection = WEST;
+  } else if (arduboy.pressed(RIGHT_BUTTON)) {
+    nDirection = EAST;
   } else if (arduboy.pressed(UP_BUTTON)) {
-    nDirection = SOUTH;
-  } else if (arduboy.pressed(DOWN_BUTTON)) {
     nDirection = NORTH;
+  } else if (arduboy.pressed(DOWN_BUTTON)) {
+    nDirection = SOUTH;
+  } else {
+    return;
   }
   //
   if (oDirection != nDirection) {
@@ -116,16 +168,16 @@ void updateDirection() {
 }
 
 /**
-   Push a new head onto the snake.
-*/
-void newSection(unsigned int direction) {
+ * Push a new section onto the snake.
+ */
+void newSection(Direction direction) {
   // First, move all existing points one
   // position down the array.
   for (int i = snake.numberOfSections; i > 0; i = i - 1) {
     snake.sections[i] = snake.sections[i - 1];
   }
   // Second, put in the new head
-  snake.sections[0] = {direction,1};
+  snake.sections[0] = {direction,0};
   // Finally, update the size of the snake
   snake.numberOfSections = snake.numberOfSections + 1;
 }
@@ -134,40 +186,79 @@ void newSection(unsigned int direction) {
    Move head of snake in current direction; trim tail as needed
 */
 void moveSnake() {
-  moveHead();
-  trimTail();
+  // First, move the head
+  snake.head = movePoint(getCurrentDirection(),snake.head);
+  // Second, update the first and last sections
+  Section *first = firstSection();
+  Section *last = lastSection();
+  lengthenSection(first);  
+  trimSection(last);
+  if(last->length == 0) {
+    // The last section of the snake has now reduced to have zero 
+    // length, in which case it can be dropped altogether.
+    removeLastSection();
+  }
 }
 
-void moveHead() {
-  int direction = snake.sections[0].direction;
-  switch (direction) {
-    case NORTH:
-      snake.head.y = snake.head.y + 1;
-      break;
-    case SOUTH:
-      snake.head.y = snake.head.y - 1;
-      break;
-    case EAST:
-      snake.head.x = snake.head.x - 1;
-      break;
-    case WEST:
-      snake.head.x = snake.head.x + 1;
-      break;
-  }
-  snake.sections[0].length = snake.sections[0].length + 1;
+Direction getCurrentDirection() {
+  return snake.sections[0].direction;
 }
 
 /**
-   Trim the tail of the snake as it moves.  The snake is made up of one of or more
-*/
-void trimTail() {
-  int last = snake.numberOfSections - 1;
-  int len = snake.sections[last].length;
-  if(len > 1) {
-    snake.sections[last].length = len-1;
-  } else {
-    snake.numberOfSections = snake.numberOfSections - 1;
+ * Move a given point in a given direction
+ */
+Point movePoint(Direction direction, Point p) {
+  switch (direction) {
+    case NORTH:
+      p.y -= 1;
+      break;
+    case SOUTH:
+      p.y += 1;
+      break;
+    case EAST:
+      p.x += 1;
+      break;
+    case WEST:
+      p.x -= 1;
+      break;
   }
+  //
+  return p;
+}
+
+/**
+ * Access the first section of the snake
+ */
+Section *firstSection() {
+  return &snake.sections[0];
+}
+
+/**
+ * Access the last section of the snake
+ */
+Section *lastSection() {
+  return &snake.sections[snake.numberOfSections-1];
+}
+
+/**
+ * Increase the length of a given section
+ */
+void lengthenSection(Section *section) {
+  section->length += 1;
+}
+
+/**
+ * Trim the tail of the snake as it moves.  The snake is made up of one of or more
+ */
+void trimSection(Section *section) {
+  section->length -= 1;
+}
+
+/**
+ * Remove the last section of the snake (e.g. because it no longer has any length)
+ */
+void removeLastSection() {  
+    snake.numberOfSections = snake.numberOfSections - 1;
 }
 
 void drawSnake() {
@@ -180,24 +271,41 @@ void drawSnake() {
   }
 }
 
-Point drawSnakeSection(Point from, Section section) {
-   Point to = from;
-   switch(section.direction) {
+Direction invert(Direction d) {
+  switch(d) {
     case NORTH:
-      to.y -= section.length;
-      break;
+      return SOUTH;
     case SOUTH:
-      to.y += section.length;
-      break;
+      return NORTH;
     case EAST:
-      to.x += section.length;
-      break;
+      return WEST;
     case WEST:    
-      to.x -= section.length;
-      break;
+      return EAST;
    }
+}
+
+Point drawSnakeSection(Point from, Section section) {
+   Point to = getEndPoint(from,section);   
    drawBlockSection(from,to);
    return to;
+}
+
+Point getEndPoint(Point start, Section section) {
+  switch(section.direction) {
+    case NORTH:
+      start.y += section.length;
+      break;
+    case SOUTH:
+      start.y -= section.length;
+      break;
+    case EAST:
+      start.x -= section.length;
+      break;
+    case WEST:    
+      start.x += section.length;
+      break;
+   }
+   return start;
 }
 
 void drawBlockSection(Point from, Point to) {
